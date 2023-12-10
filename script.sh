0697bbb783547422fba69974a9681b33536a81ef
Romain Andres <romain.andres@etu.unicaen.fr>
2023-12-10 17:41:51 +0100

    Deletion docker, nettoyage fichiers, modification gitignore pour ne pas avoir le modele entraine, et ajout de flask dans les requiremetns et modif du script shell et du readme

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 # Création d'un environnement virtuel python pour le projet
+echo "Attention, ce script va créer un environnement virtuel python pour le projet pouvant être volumieux (environ 8Go)."
 pip install virtualenv
 virtualenv ProjetIRMStack
 source ProjetIRMStack/bin/activate
 # Installation des dépendances pour lancer le code
 pip install -r requirements.txt
-# Installation de dépendances supplémentaires pour le serveur
-pip install flask
 
 
