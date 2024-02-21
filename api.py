e302849c784df88f8c39c835cd75cf1f590f8bf1
Romain Andres <romain.andres@etu.unicaen.fr>
2024-02-21 23:44:13 +0100

    ajout api performSegmentation

--- api.py
+++ api.py
@@ -65,11 +65,11 @@ def upload_rtstruct():
             files = {'file': (os.path.basename(rtstruct_path), f, 'application/dicom')}
             response = requests.post(f"{ORTHANC_URL}/instances", files=files)
         
-        # Vérifiez si la réponse contient un contenu avant de tenter de décoder le JSON
+        # verifif si la réponse contient un contenu avant de tenter de décoder le JSON
         if response.status_code in [200, 202]:
             try:
-                orthanc_response = response.json()  # Tentez de décoder le JSON uniquement si le contenu est présent
-            except ValueError:  # Gère l'absence de contenu JSON
+                orthanc_response = response.json()  # décoder le JSON uniquement si le contenu est présent
+            except ValueError:  #si absence de contenu JSON
                 orthanc_response = "No JSON content in response"
             return jsonify({"success": "RTStruct uploaded successfully", "OrthancResponse": orthanc_response}), response.status_code
         else:
@@ -78,23 +78,43 @@ def upload_rtstruct():
         print(e)
         return jsonify({"error": "Server error"}), 500
     
-"""
-@app.route('/segmentation', methods=['POST'])
-def segmentation():
-    # Simuler la réception d'images DICOM comme fichier ou comme référence
-    dicom_images = request.files.getlist('dicom_images')
+@app.route('/segmentation/<study_id>', methods=['POST'])
+def segmentation(study_id):
+    try:
+        # Récupération des instances DICOM pour l'étude spécifiée
+        response = requests.get(f"{ORTHANC_URL}/studies/{study_id}/instances")
+        if response.status_code == 200:
+            dicom_instances = response.json()
+            print("DICOM Instances for Study ID", study_id, ":", dicom_instances)
+            
+            # Ici,ajouter logique de traitement des images DICOM récupérées
+            # Pour le moment, on simule la génération du RTStruct
+            
+            rtstruct_path = simulate_rtstruct_generation()  # mock
+            
+            return upload_rtstruct_mocked(rtstruct_path)
+        else:
+            return jsonify({"error": "Failed to retrieve DICOM instances"}), response.status_code
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return jsonify({"error": "Server error"}), 500
     
-    # Ici, vous pouvez enregistrer les images DICOM si nécessaire et passer les chemins de fichier
-    # Pour le moment, nous passons simplement une liste de noms de fichiers factices
-    # à notre fonction simulate_rtstruct_generation
-    dicom_image_paths = ['image1.dcm', 'image2.dcm']  # Remplacer par les chemins réels si nécessaire
-    
-    rtstruct_path = simulate_rtstruct_generation(dicom_image_paths)
-    
-    if rtstruct_path:
-        return jsonify({"rtstruct_path": rtstruct_path}), 200
-    else:
-        return jsonify({"error": "Failed to generate RTStruct"}), 50
-"""
+def upload_rtstruct_mocked(rtstruct_path):
+    try:
+        print(f"Uploading RTStruct from {rtstruct_path}")
+        
+        with open(rtstruct_path, 'rb') as f:
+            files = {'file': (os.path.basename(rtstruct_path), f, 'application/dicom')}
+            response = requests.post(f"{ORTHANC_URL}/instances", files=files)
+        
+        if response.status_code in [200, 202]:
+            orthanc_response = response.json() if response.content else "No JSON content in response"
+            return jsonify({"success": "RTStruct uploaded successfully", "OrthancResponse": orthanc_response}), response.status_code
+        else:
+            return jsonify({"error": "Failed to upload RTStruct to Orthanc", "OrthancResponse": response.text}), response.status_code
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Server error"}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
