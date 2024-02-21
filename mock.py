38994e4fe9dcc24a82cb7166b58e36f849ff2564
Romain Andres <romain.andres@etu.unicaen.fr>
2024-02-21 23:16:15 +0100

    route pour upload un rtstruct

--- mock.py
+++ mock.py
@@ -1,4 +1,4 @@
-def simulate_rtstruct_generation(dicom_images):
+def simulate_rtstruct_generation():
     # Simulation d'un traitement des images DICOM et génération d'un RTStruct
-    rtstruct_path = './1-1.dcm'
+    rtstruct_path = '/home/romain/Documents/P_R_O_J_E_C_T_S/projetIRM/BrainMetaSegmentatorUI-Back/1-1.dcm'
     return rtstruct_path
