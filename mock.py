a704e56cfe2b6cae2f089f519efa3bafd428ba7b
Romain Andres <romain.andres@etu.unicaen.fr>
2024-02-22 07:20:45 +0100

    ajout d'une attente de 30 secondes pour simuler

--- mock.py
+++ mock.py
@@ -1,4 +1,13 @@
+import time as t
+
+
 def simulate_rtstruct_generation():
+
+    # Attendre 30 secondes pour simuler le traitement des images DICOM
+    print("Simulating RTStruct generation...")
+    t.sleep(30)
+
+
     # Simulation d'un traitement des images DICOM et génération d'un RTStruct
     rtstruct_path = '/home/romain/Documents/P_R_O_J_E_C_T_S/projetIRM/BrainMetaSegmentatorUI-Back/1-1.dcm'
     return rtstruct_path
