111000918a40cb04bc3413084f56790c5c340e18
Romain Andres <romain.andres@etu.unicaen.fr>
2024-04-10 12:11:14 +0200

    ajout version 1 de upload d'images dicoms

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
 import os
 
-from BDD.MesuresSQLite import MesuresDB
+import pydicom
 import requests
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request, jsonify, g, render_template
+from flask import Flask, g, jsonify, render_template, request
 from flask_cors import CORS
 
+from BDD.MesuresSQLite import MesuresDB
 from mock import simulate_rtstruct_generation
 
 load_dotenv()
@@ -55,30 +56,43 @@ def get_study_dicoms(study_id):
         print(e)
         return jsonify({"error": "Server error"}), 500
 
-@app.route('/uploadRTStruct', methods=['POST'])
-def upload_rtstruct():
+@app.route('/uploadDicom', methods=['POST'])
+def upload_dicom():
+    print("Requête reçue pour /uploadDicom")
+    file = request.files['file']
+    print(f"Fichier reçu : {file.filename}")
     try:
-        rtstruct_path = simulate_rtstruct_generation()
-        print(f"Uploading RTStruct from {rtstruct_path}")
-        with open(rtstruct_path, 'rb') as f:
-            files = {'file': (os.path.basename(rtstruct_path), f, 'application/dicom')}
-            response = requests.post(f"{ORTHANC_URL}/instances", files=files)
-        if response.status_code in [200, 202]:
-            # Après un envoi réussi, mettre à jour la base de données
-            db = get_db()
-            # Mock des données reçues par le modèle d'IA
-            # Ici faudra faire une bloucle for et ajouter toutes les métastases
-            data_from_ai = {"volume": 10.5, "diametre": 5.2, "slideDebut": 1, "slideFin": 10}
-            db.ajouter_metastase(**data_from_ai)
-            
-            return jsonify({"success": "RTStruct uploaded successfully"}), response.status_code
-        else:
-            return jsonify({"error": "Failed to upload RTStruct to Orthanc"}), response.status_code
-    except Exception as e:
-        print(e)
-        return jsonify({"error": "Server error"}), 500
+        # Vérifier l'extension du fichier
+        # C'est une approche basique, préférable d'analyser le contenu du fichier
+        if not file.filename.endswith('.dcm'):
+            return jsonify({"error": "Le fichier n'est pas un fichier DICOM (.dcm)"}), 400
+        
+        # Pour une validation plus robuste, tenter de lire le fichier comme un fichier DICOM
+        print(f"Tentative de lecture du fichier DICOM : {file.filename}")
+        ds = pydicom.dcmread(file)
+        if ds is None or len(ds) == 0:
+            print("Le fichier DICOM est vide ou non lisible.")
+            return jsonify({"error": "Le fichier DICOM est vide ou non lisible."}), 400
+        file.seek(0) # # Remettre le pointeur au début du fichier, c'est ultra important, sinon le pycom read empêche l'upload, sinon le fichier est à la fin et donc "vide" du point de vue de la lecture.
 
-    
+        print(f"Lecture du fichier DICOM réussie : {file.filename}")
+        # Effectuer l'upload vers Orthanc ou autre logique nécessaire
+        print(f"Préparation de l'envoi du fichier à Orthanc : {file.filename}")
+        files = {'file': (file.filename, file, 'application/dicom')}
+        response = requests.post(f"{ORTHANC_URL}/instances", files=files)
+        print(f"Réponse d'Orthanc : Statut {response.status_code}, Contenu {response.content}")
+
+        if response.status_code in [200, 202]:
+            print("DICOM file uploaded successfully")
+            return jsonify({"success": "DICOM file uploaded successfully"}), response.status_code
+        else:
+            print("Failed to upload DICOM file to Orthanc")
+            return jsonify({"error": "Failed to upload DICOM file to Orthanc"}), response.status_code
+    except Exception as e:
+        print(f"Erreur lors du traitement du fichier DICOM : {e}")
+        return jsonify({"error": "Erreur serveur"}), 500
+
+
 @app.route('/segmentation/<study_instance_uid>', methods=['POST'])
 def segmentation(study_instance_uid):
     # Convertir StudyInstanceUID en ID Orthanc
@@ -102,7 +116,7 @@ def segmentation(study_instance_uid):
         else:
             return jsonify({"error": "Failed to retrieve DICOM instances"}), response.status_code
     except requests.exceptions.RequestException as e:
-        print(e)
+        print(f"Erreur lors du traitement du fichier DICOM : {e}")
         return jsonify({"error": "Server error"}), 500
 
     
--- mock.py
+++ mock.py
@@ -10,5 +10,5 @@ def simulate_rtstruct_generation():
 
     # Simulation d'un traitement des images DICOM et génération d'un RTStruct
     # Mettez le chemin absolue de votre RTStruct ici pour simuler l'envoi
-    rtstruct_path = '/home/romain/Documents/P_R_O_J_E_T_S/projetIRM/BrainMetaSegmentatorUI-Back/rt_struct.dcm'
+    rtstruct_path = '/home/romain/Documents/P_R_O_J_E_C_T_S/projetIRM/BrainMetaSegmentatorUI-Back/1-1.dcm'
     return rtstruct_path
