5008dfc36e32e9b87c249f789681602ef1d8f433
Luca <21907062@etu.unicaen.fr>
2024-04-10 12:48:40 +0200

    Fusion de l'api avec mon travail sur la base de données et son affichage

--- BDD/MesuresSQLite.py
+++ BDD/MesuresSQLite.py
@@ -87,20 +87,61 @@ class MesuresDB:
         self.curseur.execute("INSERT INTO session (idPatient, dateObservation) VALUES (?, ?)", (id_patient, date_observation))
         self.connexion.commit()
 
+#    def afficher_sessions(self):
+#        self.curseur.execute("SELECT * FROM session")
+#        sessions = self.curseur.fetchall()
+#        for session in sessions:
+#            print("ID de la session:", session[0])
+#            print("ID du patient:", session[1])
+#            print("Date d'observation:", session[2])
+#            print("------------------------")
+#        return sessions
+    
     def afficher_sessions(self):
         self.curseur.execute("SELECT * FROM session")
         sessions = self.curseur.fetchall()
-        for session in sessions:
-            print("ID de la session:", session[0])
-            print("ID du patient:", session[1])
-            print("Date d'observation:", session[2])
-            print("------------------------")
-        return sessions
+        sessions_dict = {session[0]: {'idPatient': session[1], 'dateObservation': session[2]} for session in sessions}
+        return sessions_dict
+
 
     def supprimer_session(self, id_session):
         self.curseur.execute("DELETE FROM session WHERE idSession=?", (id_session,))
         self.curseur.execute("DELETE FROM metastase WHERE idSession=?", (id_session,))
         self.connexion.commit()
 
-    ## DÉCONNEXION
-    # Si vous n'avez pas besoin d'une méthode de déconnexion explicite, vous pouvez la supprimer de votre classe.
+    ## MÉTHODES SPÉ
+        
+#    def afficher_sessions_patient(self, id_patient):
+#        self.curseur.execute("SELECT * FROM session WHERE idPatient=?", (id_patient,))
+#        sessions = self.curseur.fetchall()
+#        for session in sessions:
+#            print("ID de la session:", session[0])
+#            print("ID du patient:", session[1])
+#            print("Date d'observation:", session[2])
+#            print("------------------------")
+#        return sessions
+#    
+#    def afficher_metastases_session(self, id_session):
+#        self.curseur.execute("SELECT * FROM metastase WHERE idSession=?", (id_session,))
+#        metastases = self.curseur.fetchall()
+#        for metastase in metastases:
+#            print("ID de la métastase:", metastase[0])
+#            print("ID de la session:", metastase[1])
+#            print("Volume:", metastase[2])
+#            print("Diamètre:", metastase[3])
+#            print("Slide début:", metastase[4])
+#            print("Slide fin:", metastase[5])
+#            print("------------------------")
+#        return metastases
+
+    def afficher_sessions_patient(self, id_patient):
+        self.curseur.execute("SELECT * FROM session WHERE idPatient=?", (id_patient,))
+        sessions = self.curseur.fetchall()
+        sessions_dict = {session[0]: {'idPatient': session[1], 'dateObservation': session[2]} for session in sessions}
+        print(sessions_dict)
+        return sessions_dict
+
+    def afficher_metastases_session(self, id_session):
+        self.curseur.execute("SELECT * FROM metastase WHERE idSession=?", (id_session,))
+        metastases = self.curseur.fetchall()
+        return metastases
--- api.py
+++ api.py
@@ -11,8 +11,162 @@ from mock import simulate_rtstruct_generation
 load_dotenv()
 
 app = Flask(__name__)
+app.static_folder = 'static'
+
+# Chemin vers la base de données SQLite contenant les données des métastases
+db_path = '/home/lucaldr/Documents/projetIRM/BrainMetaSegmentatorUI-Back/BDD/mesures.db'
+
+# Fonction pour obtenir la connexion à la base de données
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = MesuresDB(db_path)
+    return db
+
+# Route pour fermer la connexion à la base de données
+@app.teardown_appcontext
+def close_db(exc):
+    if hasattr(g, 'db'):
+        g.db.connexion.close()
+
 CORS(app)
 ORTHANC_URL = os.getenv("ORTHANC_URL")
+
+
+##### DONNÉES TEST #####
+
+with app.app_context():
+    mesures_db = get_db()
+
+    # Vérifier si des études existent déjà dans la base de données
+    etudes_existantes = mesures_db.afficher_etudes()
+    
+    # Ajouter les études uniquement si aucune étude n'existe déjà
+    if not etudes_existantes:
+        # Ajouter les études
+        mesures_db.ajouter_etude(id_patient=1, id_serie='S1', id_sop='SOP1', date_traitement='2023-01-31')
+        mesures_db.ajouter_etude(id_patient=2, id_serie='S2', id_sop='SOP2', date_traitement='2023-02-15')
+
+        # Ajouter les sessions pour la première étude
+        id_etude_1 = 1
+        mesures_db.ajouter_session(id_patient=id_etude_1, date_observation='2023-02-01')
+        mesures_db.ajouter_session(id_patient=id_etude_1, date_observation='2023-02-15')
+
+        # Ajouter les sessions pour la deuxième étude
+        id_etude_2 = 2
+        mesures_db.ajouter_session(id_patient=id_etude_2, date_observation='2023-02-20')
+        mesures_db.ajouter_session(id_patient=id_etude_2, date_observation='2023-03-05')
+
+        # Ajouter les métastases pour la première session de la première étude
+        id_session_1_etude_1 = 1
+        mesures_db.ajouter_metastase(id_session=id_session_1_etude_1, volume=10.5, diametre=5.2, slide_debut=1, slide_fin=10)
+        mesures_db.ajouter_metastase(id_session=id_session_1_etude_1, volume=10, diametre=4.7, slide_debut=15, slide_fin=25)
+
+        # Ajouter les métastases pour la deuxième session de la première étude
+        id_session_2_etude_1 = 2
+        mesures_db.ajouter_metastase(id_session=id_session_2_etude_1, volume=15.2, diametre=7.2, slide_debut=5, slide_fin=15)
+        mesures_db.ajouter_metastase(id_session=id_session_2_etude_1, volume=12.5, diametre=6.4, slide_debut=20, slide_fin=30)
+
+        # Ajouter les métastases pour la première session de la deuxième étude
+        id_session_1_etude_2 = 3
+        mesures_db.ajouter_metastase(id_session=id_session_1_etude_2, volume=8.5, diametre=4.2, slide_debut=2, slide_fin=12)
+        mesures_db.ajouter_metastase(id_session=id_session_1_etude_2, volume=7.2, diametre=3.8, slide_debut=18, slide_fin=28)
+
+        # Ajouter les métastases pour la deuxième session de la deuxième étude
+        id_session_2_etude_2 = 4
+        mesures_db.ajouter_metastase(id_session=id_session_2_etude_2, volume=11.8, diametre=6.1, slide_debut=7, slide_fin=17)
+        mesures_db.ajouter_metastase(id_session=id_session_2_etude_2, volume=9.6, diametre=5.5, slide_debut=22, slide_fin=32)
+
+##### ROUTES #####
+
+# ACCUEIL
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+## REQUETES SQLITE
+
+# AFFICHER LES ÉTUDES DE LA BDD
+@app.route('/etudes', methods=['GET'])
+def afficher_etudes():
+    with app.app_context():
+        db = get_db()
+        etudes = db.afficher_etudes()
+    return render_template('etudes.html', etudes=etudes)
+
+# AFFICHER LES SESSIONS DE LA BDD
+@app.route('/sessions', methods=['GET'])
+def afficher_sessions():
+    with app.app_context():
+        db = get_db()
+        sessions = db.afficher_sessions()
+    return render_template('sessions.html', sessions=sessions)
+
+# AFFICHER LES MÉTASTASES DE LA BDD
+@app.route('/metastases', methods=['GET'])
+def afficher_metastase():
+    with app.app_context():
+        db = get_db()
+        metastases = db.afficher_metastases()
+    return render_template('metastases.html', metastases=metastases)
+
+# AFFICHER LES SESSIONS D'UN PATIENT
+@app.route('/sessions/<int:id_patient>', methods=['GET'])
+def afficher_sessions_patient(id_patient):
+    with app.app_context():
+        db = get_db()
+        sessions_dict = db.afficher_sessions_patient(id_patient)
+    return render_template('sessions.html', sessions=sessions_dict)
+
+# AFFICHER LES MÉTASTASES PAR SESSION
+@app.route('/metastases/<int:id_session>', methods=['GET'])
+def afficher_metastases_session(id_session):
+    with app.app_context():
+        db = get_db()
+        metastases = db.afficher_metastases_session(id_session)
+    return render_template('metastases.html', metastases=metastases)
+
+# AFFICHER LA PAGE D'ARBORESCENCE
+@app.route('/arborescence')
+def arborescence():
+    with app.app_context():
+        mesures_db = get_db()
+
+        # Récupérer tous les patients de la base de données
+        patients = {}
+
+        # Récupérer toutes les études de la base de données
+        etudes = mesures_db.afficher_etudes()
+
+        # Récupérer toutes les sessions de la base de données
+        sessions = mesures_db.afficher_sessions()
+
+        # Récupérer toutes les métastases de la base de données
+        metastases = mesures_db.afficher_metastases()
+
+        # Organiser les études, sessions et métastases par patient
+        for etude in etudes:
+            patient_id = etude[1]
+            if patient_id not in patients:
+                patients[patient_id] = {'etudes': [], 'sessions': {}}
+            patients[patient_id]['etudes'].append(etude)
+
+        for session_id, session_info in sessions.items():
+            patient_id = session_info['idPatient']
+            if patient_id in patients:
+                if session_id not in patients[patient_id]['sessions']:
+                    patients[patient_id]['sessions'][session_id] = {'session': session_info, 'metastases': []}
+
+        for metastase in metastases:
+            session_id = metastase[1]
+            for patient_id, patient_data in patients.items():
+                if session_id in patient_data['sessions']:
+                    patient_data['sessions'][session_id]['metastases'].append(metastase)
+
+        return render_template('arborescence.html', patients=patients)
+
+
+## REQUETES ORTHANC
 
 @app.route('/getAllStudies', methods=['GET'])
 def get_all_studies():
@@ -144,19 +298,6 @@ def upload_rtstruct_mocked(rtstruct_path):
     except Exception as e:
         print(e)
         return jsonify({"error": "Server error"}), 500
-    
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = MesuresDB("./BDD/mesures.db")
-    print('DB:', db.connexion)
-    return db
-
-@app.teardown_appcontext
-def close_db(error):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.connexion.close()
 
 
 if __name__ == '__main__':
